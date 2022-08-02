7340ad0248f3866722aa691fcb39a91e9ad9b42f
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-08-02 12:52:59 -0400

    initial db created

---
+++ database/db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "roles" (
+  "id" int PRIMARY KEY,
+  "name" varchar(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE "users" (
+  "id" int PRIMARY KEY,
+  "first_name" varchar(100) NOT NULL,
+  "last_name" varchar(100) NOT NULL,
+  "email" varchar(100) UNIQUE NOT NULL,
+  "password" varchar(100) NOT NULL,
+  "url_image" varchar(255) UNIQUE,
+  "role_id" int
+);
+
+ALTER TABLE "users" ADD FOREIGN KEY ("role_id") REFERENCES "roles" ("id");
